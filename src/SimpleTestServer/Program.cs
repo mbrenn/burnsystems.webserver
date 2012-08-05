0b0c9fd794fa5bbcefff6beaf102180c1ef4f3c4
Martin Brenn <brenn@depon.net>
2012-08-05 19:09:40 +0200

    Documentation improvement

--- src/SimpleTestServer/Program.cs
+++ src/SimpleTestServer/Program.cs
@@ -17,7 +17,10 @@ namespace SimpleTestServer
         {
             Log.TheLog.FilterLevel = LogLevel.Verbose;
             Log.TheLog.AddLogProvider(new ConsoleProvider());
+
+            // Container storing all the plugins, activities and filters
             var activationContainer = new ActivationContainer("Server");
+
             var server = Server.CreateDefaultServer(activationContainer);
             server.AddPrefix("http://127.0.0.1:8081/");
             server.AddPrefix("http://localhost:8081/");
@@ -26,8 +29,7 @@ namespace SimpleTestServer
             server.Add(new ExceptionDispatcher(DispatchFilter.ByUrl("/exception")));
             server.Add(new FileSystemDispatcher(DispatchFilter.All, "htdocs\\"));
             server.Start();
-
-
+            
             Console.WriteLine("Press key to stop server");
             Console.ReadKey();
             server.Stop();
