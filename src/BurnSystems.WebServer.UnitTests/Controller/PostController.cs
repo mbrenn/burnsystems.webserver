e3567350da0c1602bfe9ebaaa742720062f2ea8e
Martin Brenn <brenn@depon.net>
2012-08-08 22:33:02 +0200

    Start of TemplateController

---
+++ src/BurnSystems.WebServer.UnitTests/Controller/PostController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BurnSystems.WebServer.UnitTests.Controller
+{
+    public class PostController : MVC.Controller
+    {
+        public void PostTest()
+        {
+        }
+
+    }
+}
--- src/BurnSystems.WebServer/MVC/WebMethodAttribute.cs
+++ src/BurnSystems.WebServer/MVC/WebMethodAttribute.cs
@@ -11,5 +11,13 @@ namespace BurnSystems.WebServer.MVC
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     public sealed class WebMethodAttribute : Attribute
     {
+        /// <summary>
+        /// Gets or sets the http Method being captured by the webmethod
+        /// </summary>
+        public string HttpMethod
+        {
+            get;
+            set;
+        }
     }
 }
