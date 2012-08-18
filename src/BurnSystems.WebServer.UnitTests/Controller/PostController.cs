e5c0d0cdb8cfea8d4ea4031c9f2712088822c856
Martin Brenn <brenn@depon.net>
2012-08-19 00:59:51 +0200

    Simplifies PostTest

--- src/BurnSystems.WebServer.UnitTests/Controller/PostController.cs
+++ src/BurnSystems.WebServer.UnitTests/Controller/PostController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using BurnSystems.ObjectActivation;
 using BurnSystems.WebServer.MVC;
-using BurnSystems.ObjectActivation;
-using BurnSystems.WebServer.Helper;
 using BurnSystems.WebServer.Parser;
 
 namespace BurnSystems.WebServer.UnitTests.Controller
@@ -39,14 +34,7 @@ namespace BurnSystems.WebServer.UnitTests.Controller
         [WebMethod]
         public void PostTest([PostModel] PostTestModel model, [Inject("PageTemplate")] string template)
         {
-            if (model == null)
-            {
-                this.Html(this.TemplateParser.Parse<PostTestModel>(template, null, null, "PostController"));
-            }
-            else
-            {
-                this.Html(this.TemplateParser.Parse(template, model, null, "PostController"));
-            }
+            this.Html(this.TemplateParser.Parse<PostTestModel>(template, model, null, "PostController"));
         }
     }
 }
