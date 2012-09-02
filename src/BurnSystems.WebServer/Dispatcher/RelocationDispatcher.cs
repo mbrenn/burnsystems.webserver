ed3ee88772db58d5eeed0a334c5475a87e06ed85
Martin Brenn <brenn@depon.net>
2012-09-02 17:53:01 +0200

    Encoding for StaticContentResponse

--- src/BurnSystems.WebServer/Dispatcher/RelocationDispatcher.cs
+++ src/BurnSystems.WebServer/Dispatcher/RelocationDispatcher.cs
@@ -48,6 +48,17 @@ namespace BurnSystems.WebServer.Dispatcher
         {
             this.ToUrl = new Uri(toUrl, UriKind.RelativeOrAbsolute);
         }
+
+        /// <summary>
+        /// Initializes a new instance of the RelocationDispatcher class.
+        /// </summary>
+        /// <param name="filter">Url, where relocation starts being used</param>
+        /// <param name="toUrl">Target url</param>
+        public RelocationDispatcher(Func<ContextDispatchInformation, bool> filter, string toUrl)
+            : base(filter)
+        {
+            this.ToUrl = new Uri(toUrl, UriKind.RelativeOrAbsolute);
+        }
         
         /// <summary>
         /// Dispatches the instance
--- src/BurnSystems.WebServer/Responses/StaticContentResponse.cs
+++ src/BurnSystems.WebServer/Responses/StaticContentResponse.cs
@@ -21,6 +21,15 @@ namespace BurnSystems.WebServer.Responses
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the content encoding
+        /// </summary>
+        public Encoding ContentEncoding
+        {
+            get;
+            set;
+        }
+
         public StaticContentResponse(Func<ContextDispatchInformation, bool> filter)
             : base(filter)
         {
@@ -33,10 +42,23 @@ namespace BurnSystems.WebServer.Responses
             this.Content = content;
         }
 
+        public StaticContentResponse(Func<ContextDispatchInformation, bool> filter, string contentType, string content)
+            : base(filter)
+        {
+            this.ContentType = contentType;
+            this.Content = Encoding.UTF8.GetBytes(content);
+            this.ContentEncoding = Encoding.UTF8;
+        }
+
         public override void Dispatch(ObjectActivation.IActivates activates, ContextDispatchInformation info)
         {
             info.Context.Response.ContentLength64 = this.Content.Length;
             info.Context.Response.ContentType = this.ContentType;
+
+            if (this.ContentEncoding != null)
+            {
+                info.Context.Response.ContentEncoding = this.ContentEncoding;
+            }
 
             using (var responseStream = info.Context.Response.OutputStream)
             {
