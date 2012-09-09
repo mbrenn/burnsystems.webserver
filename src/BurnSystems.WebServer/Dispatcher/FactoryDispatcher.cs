652f5e5bee445601b5af038120d11c5ee6a7a7ed
Martin Brenn <brenn@depon.net>
2012-09-09 19:31:24 +0200

    FactoryDispatcher creates new instance of Dispatcher for each request

---
+++ src/BurnSystems.WebServer/Dispatcher/FactoryDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BurnSystems.WebServer.Dispatcher
+{
+    /// <summary>
+    /// Dispatches the object by creating an instance
+    /// </summary>
+    /// <typeparam name="T">Type of the dispatchitem to be created</typeparam>
+    public class FactoryDispatcher<T> : BaseDispatcher where T : IRequestDispatcher, new()
+    {
+        public FactoryDispatcher(Func<ContextDispatchInformation, bool> filter)
+            : base(filter)
+        {
+        }
+
+        public override void Dispatch(ObjectActivation.IActivates container, ContextDispatchInformation context)
+        {
+            var instance = new T();
+            instance.Dispatch(container, context);
+            instance.FinishDispatch(container, context);
+
+            if (instance is IDisposable)
+            {
+                (instance as IDisposable).Dispose();
+            }
+        }
+    }
+}
