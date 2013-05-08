0ab1d9b6e9dc619e9c3451d29d1ffddb67e53db8
Martin Brenn <brenn@depon.net>
2013-05-09 00:33:42 +0200

    Improvements for debugging and caching of .less files

--- src/BurnSystems.WebServer/Responses/FileRequests/LessFileRequest.cs
+++ src/BurnSystems.WebServer/Responses/FileRequests/LessFileRequest.cs
@@ -1,15 +1,18 @@
-﻿using BurnSystems.WebServer.Dispatcher;
+﻿using BurnSystems.Logging;
+using BurnSystems.WebServer.Dispatcher;
+using BurnSystems.WebServer.Helper;
 using dotless.Core;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace BurnSystems.WebServer.Responses.FileRequests
 {
     class LessFileRequest : BaseDispatcher, IFileRequestDispatcher
     {
+        private ILog logger = new ClassLogger(typeof(LessFileRequest));
+
+        private static bool firstRun = true;
+
         public string PhysicalPath
         {
             get;
@@ -34,13 +37,27 @@ namespace BurnSystems.WebServer.Responses.FileRequests
         /// <param name="info">Context being used</param>
         public override void Dispatch(ObjectActivation.IActivates container, ContextDispatchInformation info)
         {
+            if (firstRun)
+            {
+                logger.LogEntry(new LogEntry("Using dotless to compile .less-files", LogLevel.Notify));
+                logger.LogEntry(new LogEntry("http://www.dotlesscss.com", LogLevel.Notify));
+                firstRun = false;
+            }
+
+            // File is not sent out at once, file is sent out by by chunks
+            if (info.CheckForCache(File.GetLastWriteTime(this.PhysicalPath), Encoding.UTF8.GetBytes(this.PhysicalPath)))
+            {
+                // File has not been modified
+                return;
+            }
+
             var lessFile = File.ReadAllText(this.PhysicalPath);
             var cssFile = Less.Parse(lessFile);
 
             var bytes = Encoding.UTF8.GetBytes(cssFile);
             info.Context.Response.ContentType = "text/css; charset=utf-8";
 
-            using ( var stream = info.Context.Response.OutputStream)
+            using (var stream = info.Context.Response.OutputStream)
             {
                 stream.Write(bytes, 0, bytes.Length);
             }
