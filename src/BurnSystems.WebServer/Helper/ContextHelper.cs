d24897dc7c472f2ca8c9004b2c867db8bd9062a8
Martin Brenn <brenn@depon.net>
2013-07-21 13:49:42 +0200

    Expiration is included

--- src/BurnSystems.WebServer/Helper/ContextHelper.cs
+++ src/BurnSystems.WebServer/Helper/ContextHelper.cs
@@ -29,6 +29,67 @@ namespace BurnSystems.WebServer.Helper
             context.Response.AddHeader("Cache-Control", "post-check=0, pre-check=0");
         }
 
+        /// <summary>
+        /// Sets that the cache gets expired by time and won't be requested from browser for the given time
+        /// </summary>
+        /// <param name="info">Information containing the context dispatch information</param>
+        /// <param name="expirationTime">Expiration date</param>
+        public static bool SetCacheExpiration(this ContextDispatchInformation info, DateTime localModificationDate, TimeSpan expirationTime, byte[] content)
+        {
+            var done = false;
+
+            localModificationDate = localModificationDate.ToUniversalTime();
+
+            var isModifiedSince = info.Context.Request.Headers["If-Modified-Since"];
+            if (isModifiedSince != null)
+            {
+                var positionSemicolon = isModifiedSince.IndexOf(";");
+
+                if (positionSemicolon != -1)
+                {
+                    isModifiedSince = isModifiedSince.Substring(0, positionSemicolon);
+                }
+
+                var cacheDate = DateTime.Parse(isModifiedSince).ToUniversalTime();
+
+                if ((localModificationDate - TimeSpan.FromSeconds(2)) < cacheDate)
+                {
+                    info.Context.Response.StatusCode = 304;
+                    done = true;
+                }
+            }
+
+            info.Context.Response.AddHeader(
+                "Last-Modified",
+                localModificationDate.ToUniversalTime().ToString("r"));
+            info.Context.Response.AddHeader(
+                "Date",
+                DateTime.Now.ToUniversalTime().ToString("r"));
+            info.Context.Response.AddHeader(
+                "Expires",
+                DateTime.Now.Add(expirationTime).ToString("r"));
+            info.Context.Response.AddHeader(
+                "Cache-Control",
+                "max-age=" + Math.Round(expirationTime.TotalSeconds).ToString());
+            info.Context.Response.AddHeader(
+                "ETag",
+                string.Format(
+                    "\"{0}\"",
+                    StringManipulation.Sha1(content)));
+
+            return done;
+        }
+
+        /// <summary>
+        /// Checks whether we browser still has a valid copy of the file within the cache, if true
+        /// 304 status response will be returned, otherwise the caller is responsible to send the payload
+        /// to the browser. 
+        /// The HTTP Headers are created, so browser will store payload in cache and rerequest if necessary. 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="localModificationDate"></param>
+        /// <param name="content"></param>
+        /// <returns></returns>
         public static bool CheckForCache(this ContextDispatchInformation info, DateTime localModificationDate, byte[] content)
         {
             try
